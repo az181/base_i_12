cf61d7276aac98d27dcc4723283213b080e6444f
az181 <94307757+az181@users.noreply.github.com>
2021-11-28 00:48:45 +0000

    fixed to problms due to merging

--- i12To_b10.py
+++ i12To_b10.py
@@ -1,18 +1,18 @@
 
+'''work inprogres on betterThanMain'''
     
-    
-def inversi12(i12Num):
-    i12Num='0'*(4-(len(i12Num)%4))+i12Num
-    dig=len(i12Num)//4
-    pos=True
-    b10Num=[0,0]
-    print(i12Num)
-    for x in enumerate(i12Num[::-1]):
-        if (x[0]//4)>=2:
-            pos=False
-        else:
-            pos=True
-        if x[0]%2:
-           b10Num[pos]=
+# def inversi12(i12Num):
+#     i12Num='0'*(4-(len(i12Num)%4))+i12Num
+#     dig=len(i12Num)//4
+#     pos=True
+#     b10Num=[0,0]
+#     print(i12Num)
+#     for x in enumerate(i12Num[::-1]):
+#         if (x[0]//4)>=2:
+#             pos=False
+#         else:
+#             pos=True
+#         if x[0]%2:
+#            b10Num[pos]=
         
         

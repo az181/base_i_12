2189a63556c2f71ab1185b65fb053bcc09b04fc2
az181 <94307757+az181@users.noreply.github.com>
2022-01-31 23:50:45 +0000

    fixed and cleaned up b10To_bi12.py

--- b10To_bi12.py
+++ b10To_bi12.py
@@ -1,8 +1,16 @@
-def b10To_b12(InNum, maxMag):
+from math import log , ceil, log1p
+from sre_constants import MAXGROUPS 
+
+errorInb10To_b12_mAndI = "Error: maxNum or InNum invalid (in b10To_b12)"
+errorInb10To_b12_mRange = "Error: invalid maxMag (in b10To_b12)"
+errorInb10To_bi12_input = "Error: invalid input (in b10To_bi12)"
+errorInb10To_bi12_magBad = "Error: invalid maxMag (in b10To_bi12)"
+
+def b10To_b12(InNum, maxMag, inproper=False):
     '''InNum is the number and maxNum is the number of digits'''
-    if not(type(maxMag) is int) and (type(InNum) is int):
-        return'maxNum or InNum invalid'
-    base12List = []
+    if not((type(maxMag) is int) and (type(InNum) is int)):
+        return errorInb10To_b12_mAndI
+    base12List = ""
     for i in range((maxMag-1), -1, -1):
         m12 = 12**i
         if InNum < m12:
@@ -10,13 +18,10 @@ def b10To_b12(InNum, maxMag):
         else:
             if 10 > InNum//m12:
                 base12List+=str(int(InNum//m12))
-            elif 10 <= InNum//m12:
-                try:
-                    base12List+=chr(InNum//m12+65-10)
-                except KeyError:
-                    return 'Error: invalid maxNum'
+            elif 10 <= InNum//m12 < 12+inproper*InNum:
+                base12List+=chr(InNum//m12+65-10)
             else:
-                return 'Error: invalid maxNum'
+                return errorInb10To_b12_mRange
             InNum = InNum % m12
     outputStr = ''
     for i in base12List:
@@ -30,7 +35,7 @@ def b10To_bi12(b10Comp, maxMag=0):
         b10ReAbs = abs(b10Comp.real)
         b10ImAbs = abs(b10Comp.imag)
     except:
-        return 'invalid input'
+        return errorInb10To_bi12_input
 
     Re = True
     Im = True
@@ -47,19 +52,14 @@ def b10To_bi12(b10Comp, maxMag=0):
             maxMag = b10ReAbs
         else:
             maxMag = b10ImAbs
-        dig = 0
-        temp = 12
-        while temp > 1:
-            temp = maxMag/(12**dig)
-            dig += 1
-        maxMag = dig
+        maxMag = ceil( log(maxMag,12))+1
 
     # converts real and imaginary to base 12
-    b12Re = b10To_b12(b10ReAbs, maxMag)
-    b12Im = b10To_b12(b10ImAbs, maxMag)
+    b12Re = b10To_b12(int(b10ReAbs), maxMag)
+    b12Im = b10To_b12(int(b10ImAbs), maxMag)
     b_i12 = ''
-    if ('Error: invalid maxNum' in b12Re) or ('Error: invalid maxNum' in b12Im):
-        return 'Error: invalid maxMag'
+    if (errorInb10To_b12_mRange in b12Re) or (errorInb10To_b12_mRange in b12Im):
+        return errorInb10To_bi12_magBad
     # puts the real and imag in the i subscript 12 base
     for x in range(maxMag*4):
         x4 = x % 4
@@ -77,10 +77,13 @@ def b10To_bi12(b10Comp, maxMag=0):
 
 
 if __name__ == '__main__':
-    # print(b102b12(12, 2))
-    # # '10'
-    # print(b102bi12(57-169j))
-    # # '100020041009'
-    # print(b102bi12(12+12j, 1))
-    # # 'Error: invalid maxMag'
+    print(b10To_b12(12, 2))
+    # '10'
+    print(b10To_b12(12330,4))
+    # '7176'
+    print(b10To_bi12(57-169j))
+    # '100020041009'
+    print(b10To_b12(40012,4,1))
+    print(b10To_bi12(12+12j, 1))
+    # 'Error: invalid maxMag (in b10To_bi12)'
     print(b10To_bi12(complex(input('input complex number here:'))))
